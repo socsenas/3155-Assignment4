c76c6464dd4e510b9abe62857146dcdf0b76b365
Simon Ocsenas <socsenas@charlotte.edu>
2025-07-08 22:51:09 -0400

    feat: add PUT endpoint with Pydantic model

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # main.py
-from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Union[str] = None
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 async def read_root():
@@ -11,3 +18,7 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name, "item_price": item.price}
